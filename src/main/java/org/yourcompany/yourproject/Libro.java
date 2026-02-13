9732d3f51f82ac40b6db7a6729b97b9710a32de7
sergiomartinez77 <sergio.martinez@gmail.com>
2026-02-12 21:53:29 -0500

    Agrego clase Libro

---
+++ src/main/java/org/yourcompany/yourproject/Libro.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Libro {
+  
+    private String isbn;
+    private String titulo;
+    private List<Ejemplar> ejemplares;
+
+    public Libro(String isbn, String titulo) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.ejemplares = new ArrayList<>();
+    }
+
+    public Ejemplar crearEjemplar(String codigo) {
+        Ejemplar nuevoEjemplar = new Ejemplar(codigo);
+        ejemplares.add(nuevoEjemplar);
+        return nuevoEjemplar;
+    }
+
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    
+    public List<Ejemplar> getEjemplares() {
+        return ejemplares;
+    }
+}
+
+
+
